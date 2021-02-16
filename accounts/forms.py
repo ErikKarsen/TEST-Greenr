de33c73871296937f2ef451f3511ad386cc97298
Erik Karsen <erik_karsen@hotmail.com>
2021-02-17 00:42:01 +0100

    CRUD actions Journeys

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 from django import forms
 
 from django.forms import ModelForm
+from .models import Journey
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class JourneyForm(ModelForm):
+    class Meta:
+        model = Journey
+        fields = ['transportation', 'duration_hours', 'duration_minutes']
