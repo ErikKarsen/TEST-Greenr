a58fa4ea59b43586f0b46588d599e58f9cfe945d
Erik Karsen <erik_karsen@hotmail.com>
2021-02-18 18:43:08 +0100

    fix reset done urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
      auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
      name='reset_password'),
 
-    path('reset_password_sent/', 
+    path('password_reset/done/', 
         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'), 
         name='password_reset_done'),
 
@@ -25,7 +25,7 @@ urlpatterns = [
      auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'), 
      name='password_reset_confirm'),
 
-    path('reset_password_complete/', 
+    path('reset/done/', 
         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'), 
         name='password_reset_complete'),
 ]
