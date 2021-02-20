34e24d52be01e17fc68375d94caf294bacdad11f
Erik Karsen <erik_karsen@hotmail.com>
2021-02-20 23:48:49 +0100

    Connect live postgres db

--- Greenr/settings.py
+++ Greenr/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'Greenr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
